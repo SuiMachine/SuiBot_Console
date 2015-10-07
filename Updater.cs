4233aadd7398dbbf140f887730afe221b10cba9c
Sui <link_matrix1@o2.pl>
2015-10-07 20:13:10 +0200

    Testing... testing...

--- Updater.cs
+++ Updater.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Xml;
+using System.ComponentModel;
 
 namespace TwitchBotConsole
 {
@@ -12,37 +13,93 @@ namespace TwitchBotConsole
         {
             try
             {
-                string uriDirectoryForFiles = "";
-                string infoUri = "update.xml";
+                string uriDirectoryForFiles = "https://raw.githubusercontent.com/SuiMachine/SuiBot_Console/Testing/UpdateDirectory/";
+                string infoUri = "update.xml"; //https://raw.githubusercontent.com/SuiMachine/SuiBot_Console/Testing/UpdateDirectory/
                 List<string> listOfFiles = new List<string>();
 
-
-                /*HttpWebRequest request = (HttpWebRequest)WebRequest.Create(infoUri);
-                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-                response.Close();*/
-
-                XmlDocument doc = new XmlDocument();
-                doc.Load(infoUri);
-                
-                foreach (XmlNode update in doc.SelectNodes("updates/update"))
+                if(true) //CheckIfXMLExists(infoUri)
                 {
-                    Version CheckedVersion = Version.Parse(update.SelectSingleNode("version").InnerText);
-                    if(CurrentVersion < CheckedVersion)
+                    XmlDocument doc = new XmlDocument();
+                    doc.Load(infoUri);
+
+                    foreach (XmlNode update in doc.SelectNodes("updates/update"))
                     {
-                        foreach (XmlNode file in update.SelectNodes("file"))
-                            if (!listOfFiles.Contains(file.InnerText)) listOfFiles.Add(file.InnerText);
+                        Version CheckedVersion = Version.Parse(update.SelectSingleNode("version").InnerText);
+                        if (CurrentVersion < CheckedVersion)
+                        {
+                            foreach (XmlNode file in update.SelectNodes("file"))
+                                if (!listOfFiles.Contains(file.InnerText)) listOfFiles.Add(file.InnerText);
+                        }
                     }
-                }
 
-                foreach(string element in listOfFiles)
+                    foreach (string element in listOfFiles)
+                    {
+                        Console.WriteLine("File: " + element);
+                    }
+
+                    bool result = downloadFiles(listOfFiles, uriDirectoryForFiles);
+                }
+                else
                 {
-                    Console.WriteLine("File" + element);
+                    Console.WriteLine("ERROR: Couldn't recieve XML file.");
                 }
             }
             catch
             {
                 Console.WriteLine("Error finding an update.");
             }
+        }
+
+        private static bool downloadFiles(List<string> listOfFiles, string sourceLocation)
+        {
+            WebClient wbClient = new WebClient();
+            wbClient.DownloadFileCompleted += WbClient_DownloadFileCompleted;
+
+            int numberOfFiles = listOfFiles.Count;
+            string[,] tempFiles = new string[numberOfFiles,2];
+            for(int i=0; i<numberOfFiles; i++)
+            {
+                tempFiles[i, 0] = listOfFiles[i];
+                tempFiles[i, 1] = Path.GetTempFileName();
+                Console.WriteLine("Update: " + tempFiles[i, 0] + " -> " + tempFiles[i, 1]);
+            }
+
+            for(int i=0; i<numberOfFiles; i++)
+            {
+                Uri tempUri;
+                Uri.TryCreate(sourceLocation + tempFiles[i, 0], UriKind.Absolute, out tempUri);
+
+                try { wbClient.DownloadFileAsync(tempUri, tempFiles[i, 1]); }
+                catch { return false; }
+            }
+
+            return false;
+        }
+
+        private static void WbClient_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            if(e.Error!=null)
+            {
+                Console.WriteLine("Error downloading file!");
+            }
+            else
+            {
+                Console.WriteLine("Downloading file completed");
+
+            }
+        }
+
+        private static bool CheckIfXMLExists(string uri)
+        {
+            try
+            {
+                HttpWebRequest req = (HttpWebRequest)WebRequest.Create(uri);
+                HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+                resp.Close();
+
+                return resp.StatusCode == HttpStatusCode.OK;
+            }
+            catch { return false; }
         }
     }
 }
